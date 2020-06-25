4f16d59e8653f8b4463a10fa4a2abf4d76950a63
davidyu7 <david.yu@yale.edu>
2020-06-25 11:45:43 -0400

    Done

--- app/controllers/instructors_controller.rb
+++ app/controllers/instructors_controller.rb
@@ -4,4 +4,10 @@ class InstructorsController < ApplicationController
     @instructors = Instructor.all
   end
 
+  def show
+    @instructor = Instructor.find(params[:id])
+    @sorted_students = @instructor.students.sort_by{|s| s.name}
+    @average_student_age = @sorted_students.map{|s| s.age}.sum / @sorted_students.count
+  end
+
 end
--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,7 +1,48 @@
 class StudentsController < ApplicationController
 
+  before_action :find_student, only: [:show, :edit, :update]
+  before_action :allowed_params, only: [:create, :update]
+
   def index
     @students = Student.all
+  end
+
+  def show
+  end
+
+  def new
+    @student = Student.new
+    @instructors = Instructor.all
+  end
+
+  def create
+    @student = Student.new(allowed_params)
+    if @student.valid?
+      @student.save
+      redirect_to instructor_path(@student.instructor)
+    else
+      flash[:errors] = @student.errors.full_messages
+      redirect_to new_student_path
+    end    
+  end
+
+  def edit
+    @instructors = Instructor.all
+  end
+
+  def update
+    @student.update(allowed_params)
+    redirect_to student_path(@student)
+  end
+
+  private
+
+  def find_student
+    @student = Student.find(params[:id])
+  end
+
+  def allowed_params
+    params.require(:student).permit(:name, :major, :age, :instructor_id)
   end
   
 end
